6f472aa981ccb8030bec2d519fbdf59ff492e10f
Harsha <harsha.v2018@gmail.com>
2025-02-21 15:36:27 +0530

    test

---
+++ src/main/java/com/AI_Travel/userservice/Dao/UserRepository.java
@@ -0,0 +1,17 @@
+package com.AI_Travel.userservice.Dao;
+import com.AI_Travel.userservice.Entities.Users;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Optional;
+
+@EnableJpaRepositories
+public interface UserRepository extends JpaRepository<Users,Integer> {
+    public Optional<Users> findUserByUserName(final String userName);
+
+    Optional<Users> findUserByUserEmail(final String userEmail);
+
+    Users findUsersByUserType(final String userType);
+
+}
